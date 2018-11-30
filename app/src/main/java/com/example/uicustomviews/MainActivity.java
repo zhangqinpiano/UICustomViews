d0656d46fc67ff140cac13787698f6ccd98a53a4
zhangqinpiano <1006682961@qq.com>
2018-11-30 17:23:52 +0800

    20181130-00

---
+++ app/src/main/java/com/example/uicustomviews/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.uicustomviews;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.Window;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
+
